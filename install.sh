2cae235c8b2d545008fbf74ba2ea18cfb9ba9cf9
David Hartunian <david@positiondev.com>
2016-10-15 13:31:59 -0400

    Add setup file to symlink my files

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+rm ~/.spacemacs
+ln -s "$(pwd)/.spacemacs" ~/
+
+rm ~/.gitconfig
+ln -s "$(pwd)/.gitconfig" ~/
+
+rm ~/.gitignore_global
+ln -s "$(pwd)/.gitignore_global" ~/
+
+rm ~/.zshrc
+ln -s "$(pwd)/.zshrc" ~/
+
+rm -rf ~/.emacs.d/private
+ln -s "$(pwd)/spacemacs-private" ~/.emacs.d/private
